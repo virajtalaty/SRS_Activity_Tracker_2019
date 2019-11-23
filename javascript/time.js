739e450990365b26546d0a26ec93334cd8ed378b
Viraj Talaty <virajtalaty@gmail.com>
2019-11-22 18:10:08 -0700

    Watch image

--- javascript/time.js
+++ javascript/time.js
@@ -1,3 +1,21 @@
 function openSettings(){  
   parent.openSettings(); 
-} 
+}
+
+function updateClock() {
+    var now = new Date(), // current date
+        months = ['January', 'February', '...']; // you get the idea
+        time = now.getHours() + ':' + now.getMinutes(), // again, you get the idea
+
+        // a cleaner way than string concatenation
+        date = [now.getDate(), 
+                months[now.getMonth()],
+                now.getFullYear()].join(' ');
+
+    // set the content of the element with the ID time to the formatted string
+    document.getElementById('time').innerHTML = [date, time].join(' / ');
+
+    // call this function again in 1000ms
+    setTimeout(updateClock, 1000);
+}
+updateClock(); // initial call 
